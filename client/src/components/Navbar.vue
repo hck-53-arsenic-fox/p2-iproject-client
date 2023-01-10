c720125aebd27e98546144b9675bc5a10e03fe42
KaisarFS <kaisar.10rpl2.2019@gmail.com>
2023-01-10 20:08:09 +0700

    Add: Home page

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<template>
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+        <div class="container-fluid">
+            <div class="d-flex align-items-center">
+                <div class="site-logo"><a href="index.html">ENBIE<span>.</span> </a></div>
+                <div class="ml-auto">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+                        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                            <li><a href="#home-section" class="nav-link">Home</a></li>
+                            <li><a href="#classes-section" class="nav-link">Classes</a></li>
+                            <li><a href="#schedule-section" class="nav-link">Schedule</a></li>
+                            <li><a href="#trainer-section" class="nav-link">Trainer</a></li>
+                            <li><a href="#services-section" class="nav-link">Services</a></li>
+                            <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle float-right"><span
+                            class="icon-menu h3"></span></a>
+                </div>
+
+            </div>
+        </div>
+
+    </header>
+</template>
---
+++ client/src/stores/player.js
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia'
+export const usePlayerStore = defineStore('player', {
+  state: () => ({
+
+  }),
+  actions: {
+
+  },
+})

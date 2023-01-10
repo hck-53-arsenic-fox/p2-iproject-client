94569d04886a2c0d0d62b848fafd45a1bbe9e099
Hardy Yuantheo <hyuantheo@gmail.com>
2023-01-10 18:07:28 +0700

    HomePage

---
+++ Client/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+
+export default{
+  components: {
+    Navbar
+  }
+}
+
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <RouterView />
+</template>
+
---
+++ Client/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../views/LandingPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: asd
+    // }
+  ]
+})
+
+export default router

d02478009592ad43f0f165226fa70401172f9f09
DwinatasariJuwita <tasyakstr@gmail.com>
2023-01-10 17:58:53 +0700

    Edit Data Transactions

--- client/src/views/formEdit.vue
+++ client/src/views/formEdit.vue
@@ -1,4 +1,40 @@
 <script>
+import { mapActions } from 'pinia';
+import { useCounterStore } from '../stores/counter';
+
+export default{
+    name: 'editForm',
+    data(){
+        return {
+            transaction:{
+                name: '',
+                date: '',
+                doctor: '',
+                id: ''
+            },
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+        ...mapActions(useCounterStore, ['detailTransactions', 'editData'])
+    },
+    async created(){
+        try {
+            const {data} = await this.detailTransactions(this.$route.params.id)
+            this.transaction.name = data.name
+            this.transaction.date = new Date(data.date)?.toLocaleDateString('en-CA')
+            this.transaction.doctor = data.Doctor.name,
+            this.transaction.id = data.id
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+
+    }
+}
 
 </script>
 
@@ -10,21 +46,21 @@
         <div class="formCreate">
             <div class="form-contents">
                 <div class="text">Formulir</div>
-                <form>
+                <form @submit.prevent="editData(transaction.id, transaction)">
                     <div class="field">
                         <span class="fas fa-user"></span>
-                        <input type="text" placeholder="input your name">
+                        <input type="text" v-model="transaction.name">
                     </div>
                     <div class="field">
                         <label class="dateform" for="date">Tanggal Pertemuan:</label>
                         <span class="fas"></span>
-                        <input type="date">
+                        <input type="date" v-model="transaction.date" >
                     </div>
                     <div class="field">
                         <label class="dateform" for="date">Doctor:</label>
                         <br>
                         <span class="fas fa-user"></span>
-                        <input type="text" placeholder="input your name">
+                        <input type="text" v-model="transaction.doctor">
                     </div>
                     <button type="submit">Submit</button>
                 </form>

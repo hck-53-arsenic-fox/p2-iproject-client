d103960d471b0e1304b841bda5b02e068e15260d
akbarridho <akbarrizqullah029@gmail.com>
2023-01-10 16:11:04 +0700

    Success payment gateway

--- client/src/stores/user.js
+++ client/src/stores/user.js
@@ -6,7 +6,8 @@ const baseUrl = 'http://localhost:3000'
 export const useUserStore = defineStore('user', {
     state: () => {
         return {
-
+            isSubs: false,
+            oneProfile: {}
         }
     },
     getters: {
@@ -43,6 +44,56 @@ export const useUserStore = defineStore('user', {
         async handleLogOut() {
             localStorage.clear()
             this.router.push('/login')
+        },
+        async changePaymentStatus() {
+            try {
+                const { data } = await axios({
+                    method: 'PATCH',
+                    url: `${baseUrl}/subscription`,
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+
+                this.oneProfile.isSubscribed = true
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async subscribe() {
+            try {
+                const { data } = await axios({
+                    method: 'POST',
+                    url: `${baseUrl}/generate-midtrans-token`,
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+
+                window.snap.pay(data.token, {
+                    onSuccess: (result) => {
+                        /* You may add your own implementation here */
+                        console.log('Success')
+                        this.changePaymentStatus()
+                    }
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async profile() {
+            try {
+                const {data} = await axios({
+                    method: 'GET',
+                    url: `${baseUrl}/profile`,
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+                this.oneProfile = data
+            } catch (error) {
+                console.log(error)
+            }
         }
     },
 })
--- client/src/views/EventView.vue
+++ client/src/views/EventView.vue
@@ -1,55 +1,32 @@
 <script>
+import { mapActions, mapState } from 'pinia';
+import { useUserStore } from '../stores/user';
+
 export default {
-    name: 'EventView'
+    name: 'EventView',
+    methods: {
+        ...mapActions(useUserStore, ['subscribe', 'profile'])
+    },
+    computed: {
+        ...mapState(useUserStore, ['isSubs', 'oneProfile'])
+    },
+    created() {
+        this.profile()
+    },
 }
 </script>
 
 <template>
-    <!---Event Option--->
-    <div class="d-flex justify-content-center" style="margin-top: 50px">
+    <!-- -Event Option--->
+    <!-- <div class="d-flex justify-content-center" style="margin-top: 50px">
         <div class="p-2">
             <a href="#" style="text-decoration: none;color: grey;font-size: 30px;font-weight: bolder;">UPCOMING</a>
         </div>
         <div class="p-2">
             <a href="#" style="text-decoration: none;color: black;font-size:30px;font-weight: bolder;">PAST</a>
         </div>
-    </div>
-    <!---End Event Option--->
-
-    <!---Upcoming Event--->
-    <div style="margin-top: 50px;">
-        <div class="d-flex justify-content-between"
-            style="text-align: center;padding-top:30px;width: 50%;margin: auto;">
-            <div class="p-2">
-                <h5>UFC Fight Night: Imavov vs. Gastelum</h5>
-                <p>2023-01-14T16:00:00</p>
-            </div>
-            <div class="p-2">
-                <button type="button" class="btn btn-primary">Buy Ticket</button>
-            </div>
-        </div>
-        <div class="d-flex justify-content-between"
-            style="text-align: center;padding-top:30px;width: 50%;margin: auto;">
-            <div class="p-2">
-                <h5>UFC Fight Night: Imavov vs. Gastelum</h5>
-                <p>2023-01-14T16:00:00</p>
-            </div>
-            <div class="p-2">
-                <button type="button" class="btn btn-primary">Buy Ticket</button>
-            </div>
-        </div>
-        <div class="d-flex justify-content-between"
-            style="text-align: center;padding-top:30px;width: 50%;margin: auto;">
-            <div class="p-2">
-                <h5>UFC Fight Night: Imavov vs. Gastelum</h5>
-                <p>2023-01-14T16:00:00</p>
-            </div>
-            <div class="p-2">
-                <button type="button" class="btn btn-primary">Buy Ticket</button>
-            </div>
-        </div>
-    </div>
-    <!---End Upcoming Event--->
+    </div> -->
+    <!---End Event Option- -->
 
     <!---Past Event--->
     <div style="margin-top: 50px;">
@@ -59,28 +36,11 @@ export default {
                 <h5>UFC Fight Night: Kattar vs. Chikadze</h5>
                 <p>2022-01-15T17:00:00</p>
             </div>
-            <div class="p-2">
-                <button type="button" class="btn btn-primary">Recap</button>
+            <div class="p-2" v-if="!oneProfile.isSubscribed">
+                <button type="button" class="btn btn-primary" @click.prevent="subscribe">Subscribe to watch</button>
             </div>
-        </div>
-        <div class="d-flex justify-content-between"
-            style="text-align: center;padding-top:30px;width: 50%;margin: auto;">
-            <div class="p-2">
-                <h5>UFC Fight Night: Kattar vs. Chikadze</h5>
-                <p>2022-01-15T17:00:00</p>
-            </div>
-            <div class="p-2">
-                <button type="button" class="btn btn-primary">Recap</button>
-            </div>
-        </div>
-        <div class="d-flex justify-content-between"
-            style="text-align: center;padding-top:30px;width: 50%;margin: auto;">
-            <div class="p-2">
-                <h5>UFC Fight Night: Kattar vs. Chikadze</h5>
-                <p>2022-01-15T17:00:00</p>
-            </div>
-            <div class="p-2">
-                <button type="button" class="btn btn-primary">Recap</button>
+            <div class="p-2" v-if="oneProfile.isSubscribed">
+                <button type="button" class="btn btn-primary">Watch</button>
             </div>
         </div>
     </div>

f334f761b589a9cef73a50fe936da7db22d4b177
DwinatasariJuwita <tasyakstr@gmail.com>
2023-01-10 14:34:20 +0700

    vuejs section

---
+++ client/src/views/formEdit.vue
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+
+<template>
+
+
+    <!-- Formulir section starts -->
+    <section class="formulir">
+        <div class="formCreate">
+            <div class="form-contents">
+                <div class="text">Formulir</div>
+                <form>
+                    <div class="field">
+                        <span class="fas fa-user"></span>
+                        <input type="text" placeholder="input your name">
+                    </div>
+                    <div class="field">
+                        <label class="dateform" for="date">Tanggal Pertemuan:</label>
+                        <span class="fas"></span>
+                        <input type="date">
+                    </div>
+                    <div class="field">
+                        <label class="dateform" for="date">Doctor:</label>
+                        <br>
+                        <span class="fas fa-user"></span>
+                        <input type="text" placeholder="input your name">
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <!-- Formulir section ends -->
+</template>
---
+++ client/src/views/navbar.vue
@@ -0,0 +1,40 @@
+<script>
+import { def } from "@vue/shared";
+import {mapActions, mapState, mapWritableState } from "pinia"
+import { useCounterStore } from "../stores/counter"
+
+export default{
+    name:"navbar",
+    computed:{
+        ...mapWritableState(useCounterStore, ['isAuth'])
+    },
+    methods:{
+        ...mapActions(useCounterStore, ['handleLogout'])
+
+    }
+}
+</script>
+
+<template>
+        <!-- header section starts-->
+        <nav>
+            <div class="nav-container">
+                <header>
+                <a class="logo"><i class='bx bx-plus-medical' ></i>Youandmie.</a>
+        
+                <ul class="navbar">
+                    <li><RouterLink to="/">Home</RouterLink></li>
+                    <li><RouterLink to="/doctors">Our Doctors</RouterLink></li>
+                    <li><RouterLink to="/about">About Us</RouterLink></li>
+                </ul>
+        
+                <div class="header-icons">
+                    <a v-if="isAuth" @click.prevent="handleLogout"><i class='bx bxs-door-open' ></i></a>
+                    <RouterLink v-if="!isAuth" to="/login"><i class='bx bxs-user'></i></RouterLink>
+                    <RouterLink to="/transactions"><i class='bx bx-basket' ></i></RouterLink>
+                </div>
+            </header>
+            </div>
+        </nav>
+    <!-- header section ends-->
+</template>

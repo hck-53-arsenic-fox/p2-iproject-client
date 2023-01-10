b3f903cff82324baff3d90a24bd643cc10221463
nabilusnup <nabilusnup@gmail.com>
2023-01-10 18:05:30 +0700

    slice component n view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterPage.vue'
+import RoomView from '../views/RoomView.vue'
+import DetailView from '../views/DetailView.vue'
+import FoodView from '../views/FoodView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +15,30 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/room',
+      name: 'room',
+      component: RoomView
+    },
+    {
+      path: '/room/:id',
+      name: 'roomDetail',
+      component: DetailView
+    },
+    {
+      path: '/food',
+      name: 'food',
+      component: FoodView
+    },
   ]
 })
 
---
+++ client/src/views/FoodView.vue
@@ -0,0 +1,45 @@
+<script>
+</script>
+
+<template>
+    <!-- DETAIL FOOD -->
+<h2 class="d-flex justify-content-center mb-5" ><span style="border-bottom: solid 1px black;">OUR MENU</span></h2>
+<div class="container row row-cols-2 row-cols-md-3 g-4" style="margin-left:5%;">
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+</div>
+</template>

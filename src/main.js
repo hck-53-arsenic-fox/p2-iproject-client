d36e43fe8a80d644d0c33464b0db49cd4ffd5755
Leonardo Tan <laurenciusltan@gmail.com>
2023-01-12 01:46:15 +0700

    register,login

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
+import vue3GoogleLogin from "vue3-google-login";
 
 import App from "./App.vue";
 import router from "./router";
@@ -9,6 +10,10 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(vue3GoogleLogin, {
+  clientId:
+    "774674133889-nrp72aef107gei08mkial8ttl7tmld5g.apps.googleusercontent.com",
+});
 
 pinia.use(({ store }) => {
   store.router = markRaw(router);
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -8,10 +8,8 @@ export const useCounterStore = defineStore("counter", {
     games: [],
     page_size: 4,
     count: 0,
+    isLogin: false,
   }),
-  getters: {
-    // showMore: (state) => state.page_size + 4,
-  },
   actions: {
     async fetchGames(search) {
       try {
@@ -36,6 +34,10 @@ export const useCounterStore = defineStore("counter", {
 
     async buyGame(slug) {
       try {
+        const { data } = await axios({
+          method: "POST",
+          url: baseUrl + "/games/" + slug,
+        });
       } catch (err) {
         Swal.fire({
           icon: "error",
@@ -43,6 +45,71 @@ export const useCounterStore = defineStore("counter", {
           text: "Something went wrong!",
         });
       }
+    },
+
+    async handleLogin(user) {
+      try {
+        const { data } = await axios({
+          url: baseUrl + "/login",
+          method: "POST",
+          data: user,
+        });
+        localStorage.setItem("access_token", data.access_token);
+        this.router.push({ name: "home" });
+        this.isLogin = true;
+      } catch (err) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.response.data.message,
+        });
+      }
+    },
+
+    async handleRegister(user) {
+      try {
+        const { data } = await axios({
+          url: baseUrl + "/register",
+          method: "POST",
+          data: user,
+        });
+        localStorage.setItem("access_token", data.access_token);
+        this.router.push({ name: "home" });
+        this.isLogin = true;
+      } catch (err) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.response.data.message,
+        });
+      }
+    },
+
+    async googleLogin(response) {
+      try {
+        const { data } = await axios({
+          url: baseUrl + "/google-login",
+          method: "POST",
+          headers: {
+            google_token: response.credential,
+          },
+        });
+        localStorage.setItem("access_token", data.access_token);
+        this.router.push({ name: "home" });
+        this.isLogin = true;
+      } catch (err) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.response.data.message,
+        });
+      }
+    },
+
+    logout() {
+      localStorage.clear();
+      this.router.push({ name: "login" });
+      this.isLogin = false;
     },
   },
 });

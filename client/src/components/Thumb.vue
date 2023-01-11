c6ae717d636a3478e7002070a05ff80ed0e88190
yopie <benonind@gmail.com>
2023-01-11 18:50:46 +0700

    menambahkan fetch data, login, register, 3rd party

---
+++ client/src/components/Thumb.vue
@@ -0,0 +1,15 @@
+<script>
+    import { useCounterStore } from '../stores/counter'
+    import { mapState, mapActions } from 'pinia'
+    export default {
+        props: ['judul','trailer','thumbnail','rating','score','rank','synopsis','background']
+    }
+</script>
+
+<template>
+        <div class="col-3">
+            <img width="220" height="350" class="rounded" :src="thumbnail"/>
+            <h3> {{ judul }} </h3>
+            <p> rating:  {{ rating }} </p>
+        </div>
+</template>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script>
+  import Card from '../components/thumb.vue'
+  import { useCounterStore } from '../stores/counter';
+  import { mapActions, mapState } from 'pinia';
+  export default {
+    components: { Card },
+    computed: {
+      ...mapState( useCounterStore, ['dataAnime'])
+    },
+  }
+</script>
+
+<template> 
+  <div class="container mt-5">
+    <div class="row">
+      <Card v-for="lop in dataAnime.data" :key="lop.mal_id"
+        :judul="lop.title"
+        :trailer="lop.trailer.url"
+        :thumbnail="lop.images.jpg.image_url"
+        :rating="lop.rating"
+        :score="lop.score"
+        :rank="lop.rank"
+        :synopsis="lop.synopsis"
+        :background="lop.background"
+      />
+    </div>
+  </div>
+</template>

3c2241c1d9063c9cd9f149a9bf38e1c6d8f6bb32
thisislin92 <herlinalim92@gmail.com>
2023-01-11 14:41:20 +0700

    Add wallets and transactions

--- client/src/views/TransactionsView.vue
+++ client/src/views/TransactionsView.vue
@@ -1,8 +1,64 @@
+<script>
+import { useAppStore } from "@/stores/app";
+import { mapState, mapActions } from "pinia";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {
+    ...mapActions(useAppStore, ["getAllTransactions"]),
+  },
+  computed: {
+    ...mapState(useAppStore, ["transactions"]),
+
+    formattedAmount() {
+      return this.transactions.amount.length > 0
+        ? this.transactions.amount.map((amount) => {
+            return amount.toLocaleString("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            });
+          })
+        : [];
+    },
+  },
+  mounted() {
+    this.getAllTransactions();
+  },
+};
+</script>
+
 <template>
   <div>
     <div class="row">
       <div class="col-12 mb-4">
         <h1>Your Transactions</h1>
+      </div>
+      <div class="col-12">
+        <table class="table table-light">
+          <thead>
+            <tr>
+              <th class="col-2">Transaction ID</th>
+              <th>Transaction Name</th>
+              <th>Transaction Amount</th>
+              <th>Date Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(transaction, index) of transactions"
+              v-bind:key="transaction.id"
+            >
+              <td>{{ transaction.id }}</td>
+              <td>{{ transaction.name }}</td>
+              <td>Rp.{{ transaction.amount }}</td>
+              <!-- <td>{{ this.formattedAmount?.[index] || "-" }}</td> -->
+              <td>{{ transaction.transactionDateTime }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>

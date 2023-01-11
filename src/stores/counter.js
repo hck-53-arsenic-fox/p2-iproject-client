27851a6492e631daf295c04b4d3ceef4698c174a
Leonardo Tan <laurenciusltan@gmail.com>
2023-01-11 18:36:43 +0700

    fetch games

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,13 +1,48 @@
 import { defineStore } from "pinia";
+import axios from "axios";
+
+let baseUrl = "http://localhost:3000";
 
 export const useCounterStore = defineStore("counter", {
-  state: () => ({ count: 0, name: "Eduardo" }),
+  state: () => ({
+    games: [],
+    page_size: 4,
+    count: 0,
+  }),
   getters: {
-    doubleCount: (state) => state.count * 2,
+    // showMore: (state) => state.page_size + 4,
   },
   actions: {
-    increment() {
-      this.count++;
+    async fetchGames(search) {
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: baseUrl + "/games",
+          params: {
+            page_size: this.page_size,
+            search,
+          },
+        });
+        this.games = data.results;
+        this.count = data.count;
+      } catch (err) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        });
+      }
+    },
+
+    async buyGame(slug) {
+      try {
+      } catch (err) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        });
+      }
     },
   },
 });

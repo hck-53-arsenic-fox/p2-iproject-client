72dc62e797132e516cb94ebb3d1a55760796c84a
flamousz <valflamous@gmail.com>
2023-01-11 11:45:53 +0700

    login template

--- client/src/stores/counter.js
+++ client/src/stores/counter.js
@@ -1,12 +1,47 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+const baseUrl = "http://localhost:3000"; 
+import axios from 'axios'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useCounterStore = defineStore('counter',  {
+    state() {
+        return {
 
-  return { count, doubleCount, increment }
+        }
+    },
+    actions: {
+        async handleGoogleLogin(val) {
+            try {
+              const { data } = await axios({
+                  method: "POST",
+                  url: `${baseUrl}/users/login`,
+                  headers: {
+                    google_token: val.credential,
+                  },
+                })
+              localStorage.setItem('access_token', data.access_token)
+              this.access_token = data.access_token
+              Toastify({
+                  text: "Welcome to Uniqli Store",
+                  style: {
+                    background: "linear-gradient(to right, #00aab0, #cdf571)",
+                  },
+      
+                  duration: 2000,
+                }).showToast();
+  
+              this.router.push('/')
+              T
+              
+            } catch (err) {
+              Toastify({
+                  text: `${err.response.data.message}`,
+                  style: {
+                    background: "linear-gradient(to right, #611302, #a62103)",
+                  },
+      
+                  duration: 2000,
+                }).showToast();
+            }  
+          }
+    }
 })
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,70 @@
+<script>
+    export default {
+    name: "LoginView",
+    };
+</script>
+
+<template>
+  <!-- Start Login -->
+  <h1>dari login</h1>
+  <section
+    id="login-section"
+    class="vh-100 bg-image"
+    style="
+      background-image: url('https://assets.mocomuseum.com/images_prod/exhibition/_2048x1536_crop_center-center_none/2162/UNTITLED-2015-244CM-HR-IMAGE.webp');
+    "
+  >
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+            <div
+              class="card bg-image"
+              style="
+                background-image: url('https://i.postimg.cc/d1P4MRgj/log.png');
+              "
+            >
+              <div class="card-body p-5">
+                <h1
+                  class="text-uppercase text-center mb-5"
+                  style="color: #c7110d"
+                >
+                  <img
+                    src="../assets/neko.svg"
+                    alt="logo"
+                    height=" 80"
+                  />
+                  <b>Uniqli Store</b>
+                </h1>
+
+
+                <div class="d-flex justify-content-center mt-3 mb-0">
+                  <p>Sign in with Google Account</p>
+                </div>
+
+                <div class="text-center text-muted mt-3 mb-0">
+                  <p style="color: rgb(54, 49, 49)">
+                    <b> Create Account Here: </b>
+                    <a
+                      href=""
+                      class="fw-bold text-body"
+                      id="login-direct"
+                      @click.prevent="this.$router.push('/register')"
+                      ><u
+                        class="bi bi-person-plus-fill"
+                        style="color: rgb(54, 49, 49)"
+                      >
+                        Sign Up</u
+                      ></a
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Login -->
+</template>

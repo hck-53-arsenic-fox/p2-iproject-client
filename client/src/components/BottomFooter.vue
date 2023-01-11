4a7753c18fa877f8280a145ee3f1bd5efd1fc60a
thisislin92 <herlinalim92@gmail.com>
2023-01-11 20:02:11 +0700

    Adding Data Deletion Policy Page

--- client/src/components/BottomFooter.vue
+++ client/src/components/BottomFooter.vue
@@ -3,5 +3,8 @@
     <div class="container">
       <Router-link to="/privacy">Privacy Policy</Router-link>
     </div>
+    <div class="container">
+      <Router-link to="/deletion">Data Deletion Policy</Router-link>
+    </div>
   </footer>
 </template>

1f3ff25b606c5e33a556c5a7e692ec94ad1c3d54
flamousz <valflamous@gmail.com>
2023-01-11 13:35:41 +0700

    navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,55 +1,80 @@
 <script>
+import { mapActions, mapState, mapWritableState } from 'pinia';
+import { useCounterStore } from '../stores/counter';
 
 export default {
-    name: 'Navbar'
-}
-
+  name: "Navbar",
+  computed: {
+    ...mapWritableState(useCounterStore, ["query"]),
+    ...mapState(useCounterStore, ['access_token'])
+  },
+  methods: {
+    ...mapActions(useCounterStore, ["handleLogout"]),
+    fetchHome() {
+      this.query = {};
+    },
+  }
+};
 </script>
 
-
-
 <template>
-
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
-  <div class="container">
-    <a class="navbar-brand" href="#"
-      ><img
-        id="NEKO-logo"
-        src="../assets/neko.svg"
-        alt="NEKO Logo"
-        draggable="false"
-        height="40"
-    />Neko Store</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto align-items-center">
-        <li class="nav-item">
-          <a class="nav-link mx-2" href="#!"><i class="fas fa-plus-circle pe-2"></i>Wishlist</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link mx-2" href="#!"><i class="fas fa-bell pe-2"></i>Cart</a>
-        </li>
-        <li class="nav-item ms-3">
-          <a class="btn btn-black btn-rounded" href="#!">Sign in</a>
-        </li>
-        <li class="nav-item ms-3">
-          <a class="btn btn-black btn-rounded" href="#!">Sign out</a>
-        </li>
-      </ul>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
+    <div class="container">
+      <RouterLink class="navbar-brand" :to="{ path: '/', query: {} }" @click.prevent="fetchHome"
+        ><img
+          id="NEKO-logo"
+          src="../assets/neko.svg"
+          alt="NEKO Logo"
+          draggable="false"
+          height="40"
+        />Neko Store</RouterLink
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto align-items-center">
+          <li class="nav-item">
+            <div v-if="access_token" >
+              <RouterLink class="nav-link mx-2" to="/wishlist"
+                ><i class="fas fa-plus-circle pe-2"></i>Wishlist</RouterLink
+              >
+            </div>
+          </li>
+          <li class="nav-item">
+            <div v-if="access_token" >
+              <RouterLink class="nav-link mx-2" to="/cart"
+                ><i class="fas fa-bell pe-2"></i>Cart</RouterLink
+              >
+            </div>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link mx-2" to="/profile"
+                ><i class="fas fa-bell pe-2"></i>Profile</RouterLink
+              >
+          </li>
+          <li class="nav-item ms-3">
+            <div v-if="!access_token"   >
+              <RouterLink class="btn btn-black btn-rounded" to="/login">Sign in</RouterLink>
+            </div>
+          </li>
+          <li class="nav-item ms-3">
+            <div v-if="access_token" >
+              <a class="btn btn-black btn-rounded" href="#!" @click.prevent="handleLogout" >Sign out</a>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-<!-- Navbar -->
-
+  </nav>
+  <!-- Navbar -->
 </template>

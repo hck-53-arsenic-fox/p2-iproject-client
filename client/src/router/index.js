1a4479e524c63c48854ff467cc8f5a74b2aadf4d
rayhanathallahariq <rayhanariq59@gmail.com>
2023-01-11 19:38:38 +0700

    addnotfound

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/home.vue'
 import Register from '../views/register.vue'
 import Detail from '../views/detail.vue'
 import Favorite from '../views/favorite.vue'
+import Nothing from '../views/nothing.vue'
 
 
 const routes= [
@@ -31,6 +32,11 @@ const routes= [
       path: '/favorite',
       name: 'favorite',
       component: Favorite
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'nothing',
+      component: Nothing
     }
   ]
   const router = createRouter({

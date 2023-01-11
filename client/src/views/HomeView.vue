599eedec27b0f9a46bab30e7a7792714c5f4d186
nabilusnup <nabilusnup@gmail.com>
2023-01-12 04:07:01 +0700

    addMaps

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,10 @@
-<script >
-</script>
+<script></script>
 
 <template>
- <section>
+  <section>
     <div
       style="
-        background-image : url('https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2018/02/27/1255/Park-Hyatt-Jakarta-P001-Location.jpg/Park-Hyatt-Jakarta-P001-Location.16x9.jpg?imwidth=1280');
+        background-image: url('https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2018/02/27/1255/Park-Hyatt-Jakarta-P001-Location.jpg/Park-Hyatt-Jakarta-P001-Location.16x9.jpg?imwidth=1280');
         min-height: 100vh;
       "
     >
@@ -44,7 +43,7 @@
     </div>
     <div class="d-flex justify-content-center mt-5 flex-column">
       <center>
-        <h1 class="" style="border-bottom: solid 2px black; width: 400px">
+        <h1 style="border-bottom: solid 2px black; width: 400px">
           Rooms & Suites
         </h1>
       </center>
@@ -113,14 +112,38 @@
               Jakarta, with 220 oversized rooms, including 36 suites within our
               property.
             </p>
-            <button class="btn btn-outline-secondary" @click.prevent="this.$router.push('/room')">View More</button>
+            <button
+              class="btn btn-outline-secondary"
+              @click.prevent="this.$router.push('/room')"
+            >
+              View More
+            </button>
           </div>
         </div>
       </div>
     </div>
-    <div>
-      
+    <div class="card mb-5 mt-5 container" style="max-width: 1000px; padding: 0">
+      <div class="row g-0">
+        <div class="col-md-4 mt-5">
+          <div class="card-body mt-3">
+            <h5 class="card-title" style="border-bottom: solid 1px black; width: 150px">Our Location</h5>
+            <p class="card-text mt-4">Map, Parking + Transportation</p>
+            <p class="card-text">Email : BlackDoorz@mail.com</p>
+            <p class="card-text">Phone : +62 21 3111 1234</p>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.9407322516167!2d110.36029411392403!3d-7.796100079538128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5993032c838b%3A0xf690bedfda29d738!2sBLACKDOOR%20Lounge%20%26%20KTV!5e0!3m2!1sid!2sid!4v1673469848325!5m2!1sid!2sid"
+            width="666"
+            height="300"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.9407322516167!2d110.36029411392403!3d-7.796100079538128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5993032c838b%3A0xf690bedfda29d738!2sBLACKDOOR%20Lounge%20%26%20KTV!5e0!3m2!1sid!2sid!4v1673469848325!5m2!1sid!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </section>
 </template>

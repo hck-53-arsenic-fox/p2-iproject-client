4b46f5b29ad58ef2a18c7a876637fa8481cdecc3
rezadulhaq <dhiaulhaqreza@gmail.com>
2023-01-12 12:09:31 +0700

    update link

--- client/src/stores/counter.js
+++ client/src/stores/counter.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-const baseUrl = "flimsy-bedroom-production.up.railway.app";
+const baseUrl = "https://flimsy-bedroom-production.up.railway.app";
 export const useCounterStore = defineStore("counter", {
   state() {
     return {

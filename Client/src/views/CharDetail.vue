7905a9e30d868290727b2388a50d6dcfda6b2dee
Hardy Yuantheo <hyuantheo@gmail.com>
2023-01-12 10:54:13 +0700

    styling

--- Client/src/views/CharDetail.vue
+++ Client/src/views/CharDetail.vue
@@ -20,23 +20,28 @@
 
 <template>
     <body>
-        <div id="side-pic">
+        <div id="side-pic" >
             <img :src="chara.image"/>
         </div>
-        <div id="content-detail" style="border-style:solid; border-radius:15px; padding: 50px 50px; background-color:white">
+        <div id="content-detail" style="border-style:solid; border-radius:15px; padding: 50px 50px; background-color:white; width: 90%; align-items: center; margin-left: 5%;">
             <h2>{{ chara.name }}</h2>
             <h5>{{ chara.description }}</h5>
+            <br/>
             <h4>Talents:</h4>
             <div v-for="talents in chara.talents" :key="talents.id">
-                <img :src="talents.icon"/>
+                <img :src="talents.icon" style="border-style: solid; border-radius: 15px;background-color: #001133;"/>
+                <br/>
                 <h5>{{ talents.name }}</h5>
                 <h6>{{ talents.description }}</h6>
+                <br/>
             </div>
             <h4>Skills:</h4>
             <div v-for="skills in chara.skills" :key="skills.id">
-                <img :src="skills.icon"/>
+                <img :src="skills.icon" style="border-style: solid; border-radius: 15px;background-color: #001133;"/>
+                <br/>
                 <h5>{{ skills.name }}</h5>
                 <h6>{{ skills.description }}</h6>
+                <br/>
             </div>
         </div>
     </body>
--- Client/src/views/SearchAccount.vue
+++ Client/src/views/SearchAccount.vue
@@ -28,27 +28,27 @@
                 },
                 deep: true
             }
-        },
-
-        
+        },   
     }
 </script>
 
 <template>
-    <body style="background-image: url(https://wallpaperaccess.com/full/3628936.jpg); background-repeat: no-repeat; background-size: contain;">
+    <body style="background-image: url(https://wallpaperaccess.com/full/3628936.jpg); background-size: cover; background-position: center; background-repeat: no-repeat; min-height: 1080px; ">
         <h1 style="color: white; display: flex; justify-content: center; padding-top: 50px;">Find a Traveller</h1><br/>
         <form id="acc-search" @submit.prevent="this.$router.push(`?uid=${query}`)">
             <input type="text" style="width: 500px; height: 35px; text-align: center;" placeholder="Insert UID here.." v-model="query"/>
             <input type="submit" style="height: 35px" value="Search UID">
         </form>
-        <div>
-            <img :src="account.Namecard" style="width: 750px"/>
-            <img :src="account.PP" style="width: 125px;"/>
-                <p>{{ account.Name }}</p>
-                <p>{{ account.Level }}</p>
-                <p>{{ account.WL }}</p>
-                <p>{{ account.Achievements }}</p>
-            <img :src="account.splashImg" style="width: 1000px"/>
+        <div id = player-card style="display: flex;">
+            <!-- <img :src="account.Namecard" style="width: 1000px;"/> -->
+            <img :src="account.PP" style="margin-top: 250px; border-style: solid; border-radius: 15px; background-color: white; margin-left: 25px;"/>
+            <div style="margin-top: 250px; margin-left: 25px; border-style: solid; border-radius: 15px; padding-right: 250px; background-color: white ;">
+                <h5 style="margin-bottom: 15px; font-family: Verdana, Geneva, Tahoma, sans-serif; font-size-adjust: 550; margin-left: 25px; margin-top: 25px">Nickname: {{ account.Name }}</h5>
+                <h5 style="margin-bottom: 15px; font-family: Verdana, Geneva, Tahoma, sans-serif; font-size-adjust: 550;margin-left: 25px;">Level: {{ account.Level }}</h5>
+                <h5 style="margin-bottom: 15px; font-family: Verdana, Geneva, Tahoma, sans-serif; font-size-adjust: 550;margin-left: 25px;">World Level: {{ account.WL }}</h5>
+                <h5 style="margin-bottom: 15px; font-family: Verdana, Geneva, Tahoma, sans-serif; font-size-adjust: 550;margin-left: 25px;">Achievements: {{ account.Achievements }}</h5>
+            </div>
+            <img :src="account.splashImg" style="width: 1000px; margin-top: 75px;"/>
         </div>
     </body>
 </template>
